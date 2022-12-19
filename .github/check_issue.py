55cb9c325247653a4df16166fe8d176220157f56
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-12-19 10:25:23 +0100

    Update check_issue.py

--- .github/check_issue.py
+++ .github/check_issue.py
@@ -5,7 +5,7 @@ import difflib
 
 import os
 
-THRESHOLD = 0.9
+THRESHOLD = 0.95
 DB_URL = "https://raw.githubusercontent.com/recloudstream/cs-repos/master/repos-db.json"
 
 def remove_suffix(text, suffix):
